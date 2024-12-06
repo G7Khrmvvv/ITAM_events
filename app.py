9563fc2ad866a9e7d233cdd1723f3c08e2955a8b
G7Khrmvvv <khramovgleb40@gmail.com>
2024-12-06 17:54:44 +0300

    add html files and css start work on database

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+
+
+@app.route('/event')
+def event_page():
+    return render_template("event.html")
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def calendar_event():
+    return render_template("calendar.html")
 
 
 if __name__ == '__main__':
