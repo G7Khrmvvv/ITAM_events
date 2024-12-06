a094fee7ddcbaef0b98cf17cfae491227791e6f2
G7Khrmvvv <khramovgleb40@gmail.com>
2024-12-06 22:08:03 +0300

    write "Something goes wrong" after post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,17 +8,17 @@ db = SQLAlchemy(app)
 class Calendar(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(15), nullable=False) # 15 ставил от балды, фронтдендеру или дизайнеру надо поменять
-    description = db.Column(db.Text, nullable=True)
+    #description = db.Column(db.Text, nullable=True)
     category = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(50), nullable=False)
-    locations = db.Column(db.String(50), nullable=True)
-    tags = db.Column(db.String(100), nullable=True)
-    period = db.Column(db.String(50), nullable=True)
-    duration = db.Column(db.String(50), nullable=True)
+    #locations = db.Column(db.String(50), nullable=True)
+    #tags = db.Column(db.String(100), nullable=True)
+    #period = db.Column(db.String(50), nullable=True)
+    #duration = db.Column(db.String(50), nullable=True)
     start_datetime = db.Column(db.String(50), nullable=False)
     end_datetime = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.String(50), nullable=True)
-    updated_at = db.Column(db.String(50), nullable=True)
+    #created_at = db.Column(db.String(50), nullable=True)
+    #updated_at = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
         return '<Calendar %r>' % self.id
@@ -41,8 +41,22 @@ def about_page():
 
 @app.route('/add_event', methods=['POST', 'GET'])
 def event_creation():
-    if (request.method == "POST"):
-        title = request.form['title'] # EDIT HEREEEEEEE
+    if request.method == "POST":
+        name = request.form['name']
+        category = request.form['category']
+        status = request.form['status']
+        start = request.form['start_datetime']
+        end = request.form['end_datetime']
+
+        calendar = Calendar(name=name, category=category, status=status, start_datetime=start, end_datetime=end)
+
+        try:
+            db.session.add(calendar)
+            db.session.commit()
+            return redirect("/calendar")
+        except:
+            return "Something goes wrong"
+
     else:
         return render_template("add_event.html")
 
