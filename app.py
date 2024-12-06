65f92ae471b426dabb1ac499c7e8fb08c9d0db6d
G7Khrmvvv <khramovgleb40@gmail.com>
2024-12-06 19:43:41 +0300

    add some frontend

--- app.py
+++ app.py
@@ -3,7 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+db = SQLAlchemy(app)
 
+# class Calendar(db.Model):
+#     id = db.Column(db.Integer)
+#     name = db.Column(db.String(15), nullable=False) # 15 ставил от балды, фронтдендеру или дизайнеру надо поменять
+#     description = db.Column(db.Text, nullable=True)
+#     category = db.Column(db.String(100), nullable=False)
+#     status = db.Column(db.String(50), nullable=False)
+#     locations = db.Column(db.String(50), nullable=True)
+#     tags = db.Column(db.String(100), nullable=True)
+#     period = db.Column(db.String(50), nullable=True)
+#     duration = db.Column(db.String(50), nullable=True)
+#     start_datetime = db.Column(db.String(50), nullable=False)
+#     end_datetime = db.Column(db.String(50), nullable=False)
+#     created_at = db.Column(db.String(50), nullable=True)
+#     updated_at = db.Column(db.String(50), nullable=True)
+#
+#     def __repr__(self):
+#         return '<Article %r>' % self.id
 
 @app.route('/event')
 def event_page():
